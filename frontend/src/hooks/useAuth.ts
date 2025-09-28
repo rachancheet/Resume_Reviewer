ec19826b1a9ac0c840eda6a7b399bfd0facaee21
rachancheet <rachancheet37@gmail.com>
2025-09-28 22:11:09 +0530

    Fix:2

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -108,13 +108,13 @@ export function useAuth() {
 
     const getURL = () => {
       let url = process?.env?.NEXT_PUBLIC_SITE_URL ?? process?.env?.NEXT_PUBLIC_VERCEL_URL ??'http://localhost:3000/'
-      
+
       url = url.startsWith('http') ? url : `https://${url}`
       url = url.endsWith('/') ? url : `${url}/`
       return url
     }
     
-
+console.log("email redirect to ",getURL())
     const { error } = await supabase.auth.signInWithOtp({
       email,
       options: {
